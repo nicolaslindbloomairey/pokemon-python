8268ddc7bc77375e333722c172225acd495a35a9
Nicolas Lindbloom-Airey <nicolas.lindbloom.airey@gmail.com>
2018-05-18 10:28:25 -0700

    mega test

--- test/test_mega.py
+++ test/test_mega.py
@@ -6,28 +6,39 @@ class TestMega(unittest.TestCase):
 
     def test_pidgeot(self):
         battle = Battle(debug=False, rng=False)
-        battle.join(0, [{'species': 'pidgeot', 'item': 'pidgeotite', 'moves': ['tackle', 'protect']}])
+        battle.join(0, [{'species': 'pidgeot',
+                         'item': 'pidgeotite',
+                         'moves': ['tackle', 'protect']}])
         battle.join(1, [{'species': 'mew', 'moves': ['tackle']}])
 
         battle.choose(0, dex.Decision('move', 0, mega=True))
         battle.choose(1, dex.Decision('move', 0, mega=True))
         battle.do_turn()
 
-        self.assertEqual(battle.sides[0].active_pokemon.species, 'pidgeotmega')
-        self.assertEqual(battle.sides[0].active_pokemon.hp, battle.sides[0].active_pokemon.maxhp-23)
+        pidgeot = battle.sides[0].active_pokemon
+        self.assertEqual(pidgeot.species, 'pidgeotmega')
+        self.assertEqual(pidgeot.hp, pidgeot.maxhp-23)
 
     def test_mewtwo_x(self):
         battle = Battle(debug=False, rng=False)
-        battle.join(0, [{'species': 'mewtwo', 'item': 'mewtwonitex', 'moves': ['tackle', 'protect']}])
-        battle.join(1, [{'species': 'charizard', 'item': 'charizarditex', 'moves': ['tackle']}])
+        battle.join(0, [{'species': 'mewtwo',
+                         'item': 'mewtwonitex',
+                         'moves': ['tackle', 'protect']
+                         }])
+        battle.join(1, [{'species': 'charizard',
+                         'item': 'charizarditex',
+                         'moves': ['tackle']
+                         }])
 
         battle.choose(0, dex.Decision('move', 0, mega=True))
         battle.choose(1, dex.Decision('move', 0, mega=True))
         battle.do_turn()
 
-        self.assertEqual(battle.sides[0].active_pokemon.species, 'mewtwomegax')
-        self.assertEqual(battle.sides[1].active_pokemon.species, 'charizarditex')
-        self.assertEqual(battle.sides[0].active_pokemon.hp, battle.sides[0].active_pokemon.maxhp-30)
+        mewtwo = battle.sides[0].active_pokemon
+        charizard = battle.sides[1].active_pokemon
+        self.assertEqual(mewtwo.species, 'mewtwomegax')
+        self.assertEqual(charizard.species, 'charizarditex')
+        self.assertEqual(mewtwo.hp, mewtwo.maxhp-30)
     
     def runTest(self):
         self.test_pidgeot()
